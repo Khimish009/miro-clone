12172ff1fd3095b74dbbdf9deb22d2f10099913c
Сальман Химишев <salman.khimishev01@gmail.com>
2025-05-18 00:09:53 +0300

    feat(config): add api

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,7 +1,15 @@
+import { rqClient } from "@/shared/api/instance";
+import { ROUTES } from "@/shared/model/routes";
+import { href, Link } from "react-router-dom";
+
 function BoardsListPage() {
+  const boardsQuery = rqClient.useQuery("get", "/boards");
   return (
     <div>
       <h1>Boards list</h1>
+      {boardsQuery.data?.map((board) => (
+        <Link to={href(ROUTES.BOARD, { boardId: board.id })}>{board.name}</Link>
+      ))}
     </div>
   );
 }
