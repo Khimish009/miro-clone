5d6ee301cf64606f0388a153e8f0ab66f6e51ae0
Сальман Химишев <salman.khimishev01@gmail.com>
2025-05-19 23:10:00 +0300

    feat(session): add session mocks

--- src/shared/api/mocks/handlers/boards.ts
+++ src/shared/api/mocks/handlers/boards.ts
@@ -1,6 +1,7 @@
 import { HttpResponse } from "msw";
 import { http } from "../http";
 import type { ApiSchemas } from "../../schema";
+import { verifyTokenOrThrow } from "../session";
 
 const boards: ApiSchemas["Board"][] = [
   {
@@ -14,7 +15,31 @@ const boards: ApiSchemas["Board"][] = [
 ];
 
 export const boardsHandlers = [
-  http.get("/boards", () => {
+  http.get("/boards", async (ctx) => {
+    await verifyTokenOrThrow(ctx.request);
     return HttpResponse.json(boards);
+  }),
+  http.post("/boards", async (ctx) => {
+    await verifyTokenOrThrow(ctx.request);
+    const data = await ctx.request.json();
+    const board = {
+      id: crypto.randomUUID(),
+      name: data.name,
+    };
+    boards.push(board);
+    return HttpResponse.json(board);
+  }),
+  http.delete("/boards/{boardId}", async ({ params, request }) => {
+    await verifyTokenOrThrow(request);
+    const { boardId } = params;
+    const index = boards.findIndex((board) => board.id === boardId);
+    if (index === -1) {
+      return HttpResponse.json(
+        { message: "Board not found", code: "NOT_FOUND" },
+        { status: 404 },
+      );
+    }
+    boards.splice(index, 1);
+    return HttpResponse.json({ message: "Board deleted", code: "OK" });
   }),
 ];
