b3704c936593d1611b2dcf3087c01c7bc7c1b850
Сальман Химишев <salman.khimishev01@gmail.com>
2025-05-22 21:53:03 +0300

    chore(header): refactor

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,8 @@
-import { AppHeader } from "@/features/header";
-import { ROUTES } from "@/shared/model/routes";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export function App() {
-  const { pathname } = useLocation();
-
-  const isAuthPage = pathname === ROUTES.LOGIN || pathname === ROUTES.REGISTER;
-
   return (
     <div className="min-h-screen flex flex-col">
-      {!isAuthPage && <AppHeader />}
       <Outlet />
     </div>
   );
