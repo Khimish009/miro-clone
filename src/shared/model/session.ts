05ec5de3e01ebc60aba14ad877156c37eda67ee1
Сальман Химишев <salman.khimishev01@gmail.com>
2025-05-22 21:19:32 +0300

    feat(session): add session model

---
+++ src/shared/model/session.ts
@@ -0,0 +1,30 @@
+import { decodeJwt } from "jose";
+import { useState } from "react";
+import { createGStore } from "create-gstore";
+
+type Session = {
+  userId: string;
+  email: string;
+  exp: number;
+  iat: number;
+};
+
+const TOKEN_KEY = "token";
+
+export const useSession = createGStore(() => {
+  const [token, setToken] = useState(() => localStorage.getItem(TOKEN_KEY));
+
+  const login = (token: string) => {
+    setToken(token);
+    localStorage.setItem(TOKEN_KEY, token);
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem(TOKEN_KEY);
+  };
+
+  const session = token ? decodeJwt<Session>(token) : null;
+
+  return { login, logout, session };
+});
